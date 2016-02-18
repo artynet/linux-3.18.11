e8a8dd3db988054c937a82a48eee6b6a96aba995
Arturo Rinaldi <arty.net2@gmail.com>
2016-02-18 15:56:04 +0100

    adding st95hf nfc driver

---
+++ include/linux/fwnode.h
@@ -0,0 +1,156 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+<title>kernel/git/stable/linux-stable.git - Linux kernel stable tree</title>
+<meta name='generator' content='cgit v0.12'/>
+<meta name='robots' content='index, nofollow'/>
+<link rel='stylesheet' type='text/css' href='/cgit-korg-0.10.1.css'/>
+<link rel='shortcut icon' href='/favicon.png'/>
+<link rel='alternate' title='Atom feed' href='http://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/atom/include/linux/fwnode.h?h=master' type='application/atom+xml'/>
+<link rel='vcs-git' href='git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git' title='kernel/git/stable/linux-stable.git Git repository'/>
+<link rel='vcs-git' href='https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git' title='kernel/git/stable/linux-stable.git Git repository'/>
+<link rel='vcs-git' href='https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux-stable.git' title='kernel/git/stable/linux-stable.git Git repository'/>
+</head>
+<body>
+<div id='cgit'><table id='header'>
+<tr>
+<td class='logo' rowspan='2'><a href='/cgit/'><img src='/tux-cgit.png' alt='cgit logo'/></a></td>
+<td class='main'><a href='/cgit/'>index</a> : <a title='kernel/git/stable/linux-stable.git' href='/cgit/linux/kernel/git/stable/linux-stable.git/'>kernel/git/stable/linux-stable.git</a></td><td class='form'><form method='get' action=''>
+<input type='hidden' name='id' value='refs/tags/v4.4.1'/><select name='h' onchange='this.form.submit();'>
+<option value='linux-2.6.11.y'>linux-2.6.11.y</option>
+<option value='linux-2.6.12.y'>linux-2.6.12.y</option>
+<option value='linux-2.6.13.y'>linux-2.6.13.y</option>
+<option value='linux-2.6.14.y'>linux-2.6.14.y</option>
+<option value='linux-2.6.15.y'>linux-2.6.15.y</option>
+<option value='linux-2.6.16.y'>linux-2.6.16.y</option>
+<option value='linux-2.6.17.y'>linux-2.6.17.y</option>
+<option value='linux-2.6.18.y'>linux-2.6.18.y</option>
+<option value='linux-2.6.19.y'>linux-2.6.19.y</option>
+<option value='linux-2.6.20.y'>linux-2.6.20.y</option>
+<option value='linux-2.6.21.y'>linux-2.6.21.y</option>
+<option value='linux-2.6.22.y'>linux-2.6.22.y</option>
+<option value='linux-2.6.23.y'>linux-2.6.23.y</option>
+<option value='linux-2.6.24.y'>linux-2.6.24.y</option>
+<option value='linux-2.6.25.y'>linux-2.6.25.y</option>
+<option value='linux-2.6.26.y'>linux-2.6.26.y</option>
+<option value='linux-2.6.27.y'>linux-2.6.27.y</option>
+<option value='linux-2.6.28.y'>linux-2.6.28.y</option>
+<option value='linux-2.6.29.y'>linux-2.6.29.y</option>
+<option value='linux-2.6.30.y'>linux-2.6.30.y</option>
+<option value='linux-2.6.31.y'>linux-2.6.31.y</option>
+<option value='linux-2.6.32.y'>linux-2.6.32.y</option>
+<option value='linux-2.6.33.y'>linux-2.6.33.y</option>
+<option value='linux-2.6.34.y'>linux-2.6.34.y</option>
+<option value='linux-2.6.35.y'>linux-2.6.35.y</option>
+<option value='linux-2.6.36.y'>linux-2.6.36.y</option>
+<option value='linux-2.6.37.y'>linux-2.6.37.y</option>
+<option value='linux-2.6.38.y'>linux-2.6.38.y</option>
+<option value='linux-2.6.39.y'>linux-2.6.39.y</option>
+<option value='linux-3.0.y'>linux-3.0.y</option>
+<option value='linux-3.1.y'>linux-3.1.y</option>
+<option value='linux-3.10.y'>linux-3.10.y</option>
+<option value='linux-3.11.y'>linux-3.11.y</option>
+<option value='linux-3.12.y'>linux-3.12.y</option>
+<option value='linux-3.13.y'>linux-3.13.y</option>
+<option value='linux-3.14.y'>linux-3.14.y</option>
+<option value='linux-3.15.y'>linux-3.15.y</option>
+<option value='linux-3.16.y'>linux-3.16.y</option>
+<option value='linux-3.17.y'>linux-3.17.y</option>
+<option value='linux-3.18.y'>linux-3.18.y</option>
+<option value='linux-3.19.y'>linux-3.19.y</option>
+<option value='linux-3.2.y'>linux-3.2.y</option>
+<option value='linux-3.3.y'>linux-3.3.y</option>
+<option value='linux-3.4.y'>linux-3.4.y</option>
+<option value='linux-3.5.y'>linux-3.5.y</option>
+<option value='linux-3.6.y'>linux-3.6.y</option>
+<option value='linux-3.7.y'>linux-3.7.y</option>
+<option value='linux-3.8.y'>linux-3.8.y</option>
+<option value='linux-3.9.y'>linux-3.9.y</option>
+<option value='linux-4.0.y'>linux-4.0.y</option>
+<option value='linux-4.1.y'>linux-4.1.y</option>
+<option value='linux-4.2.y'>linux-4.2.y</option>
+<option value='linux-4.3.y'>linux-4.3.y</option>
+<option value='linux-4.4.y'>linux-4.4.y</option>
+<option value='master' selected='selected'>master</option>
+</select> <input type='submit' name='' value='switch'/></form></td></tr>
+<tr><td class='sub'>Linux kernel stable tree</td><td class='sub right'>Greg Kroah-Hartman</td></tr></table>
+<table class='tabs'><tr><td>
+<a href='/cgit/linux/kernel/git/stable/linux-stable.git/'>summary</a><a href='/cgit/linux/kernel/git/stable/linux-stable.git/refs/?id=refs/tags/v4.4.1'>refs</a><a href='/cgit/linux/kernel/git/stable/linux-stable.git/log/include/linux/fwnode.h'>log</a><a class='active' href='/cgit/linux/kernel/git/stable/linux-stable.git/tree/include/linux/fwnode.h?id=refs/tags/v4.4.1'>tree</a><a href='/cgit/linux/kernel/git/stable/linux-stable.git/commit/include/linux/fwnode.h?id=refs/tags/v4.4.1'>commit</a><a href='/cgit/linux/kernel/git/stable/linux-stable.git/diff/include/linux/fwnode.h?id=refs/tags/v4.4.1'>diff</a><a href='/cgit/linux/kernel/git/stable/linux-stable.git/stats/include/linux/fwnode.h'>stats</a></td><td class='form'><form class='right' method='get' action='/cgit/linux/kernel/git/stable/linux-stable.git/log/include/linux/fwnode.h'>
+<input type='hidden' name='id' value='refs/tags/v4.4.1'/><select name='qt'>
+<option value='grep'>log msg</option>
+<option value='author'>author</option>
+<option value='committer'>committer</option>
+<option value='range'>range</option>
+</select>
+<input class='txt' type='text' size='10' name='q' value=''/>
+<input type='submit' value='search'/>
+</form>
+</td></tr></table>
+<div class='path'>path: <a href='/cgit/linux/kernel/git/stable/linux-stable.git/tree/?id=refs/tags/v4.4.1'>root</a>/<a href='/cgit/linux/kernel/git/stable/linux-stable.git/tree/include?id=refs/tags/v4.4.1'>include</a>/<a href='/cgit/linux/kernel/git/stable/linux-stable.git/tree/include/linux?id=refs/tags/v4.4.1'>linux</a>/<a href='/cgit/linux/kernel/git/stable/linux-stable.git/tree/include/linux/fwnode.h?id=refs/tags/v4.4.1'>fwnode.h</a></div><div class='content'>blob: 8516717427906948a215c56fad35ab462a8cbfda (<a href='/cgit/linux/kernel/git/stable/linux-stable.git/plain/include/linux/fwnode.h?id=refs/tags/v4.4.1'>plain</a>)
+<table summary='blob content' class='blob'>
+<tr><td class='linenumbers'><pre><a id='n1' href='#n1'>1</a>
+<a id='n2' href='#n2'>2</a>
+<a id='n3' href='#n3'>3</a>
+<a id='n4' href='#n4'>4</a>
+<a id='n5' href='#n5'>5</a>
+<a id='n6' href='#n6'>6</a>
+<a id='n7' href='#n7'>7</a>
+<a id='n8' href='#n8'>8</a>
+<a id='n9' href='#n9'>9</a>
+<a id='n10' href='#n10'>10</a>
+<a id='n11' href='#n11'>11</a>
+<a id='n12' href='#n12'>12</a>
+<a id='n13' href='#n13'>13</a>
+<a id='n14' href='#n14'>14</a>
+<a id='n15' href='#n15'>15</a>
+<a id='n16' href='#n16'>16</a>
+<a id='n17' href='#n17'>17</a>
+<a id='n18' href='#n18'>18</a>
+<a id='n19' href='#n19'>19</a>
+<a id='n20' href='#n20'>20</a>
+<a id='n21' href='#n21'>21</a>
+<a id='n22' href='#n22'>22</a>
+<a id='n23' href='#n23'>23</a>
+<a id='n24' href='#n24'>24</a>
+<a id='n25' href='#n25'>25</a>
+<a id='n26' href='#n26'>26</a>
+<a id='n27' href='#n27'>27</a>
+<a id='n28' href='#n28'>28</a>
+<a id='n29' href='#n29'>29</a>
+</pre></td>
+<td class='lines'><pre><code><span class="hl com">/*</span>
+<span class="hl com"> * fwnode.h - Firmware device node object handle type definition.</span>
+<span class="hl com"> *</span>
+<span class="hl com"> * Copyright (C) 2015, Intel Corporation</span>
+<span class="hl com"> * Author: Rafael J. Wysocki &lt;rafael.j.wysocki&#64;intel.com&gt;</span>
+<span class="hl com"> *</span>
+<span class="hl com"> * This program is free software; you can redistribute it and/or modify</span>
+<span class="hl com"> * it under the terms of the GNU General Public License version 2 as</span>
+<span class="hl com"> * published by the Free Software Foundation.</span>
+<span class="hl com"> */</span>
+
+<span class="hl ppc">#ifndef _LINUX_FWNODE_H_</span>
+<span class="hl ppc">#define _LINUX_FWNODE_H_</span>
+
+<span class="hl kwb">enum</span> fwnode_type <span class="hl opt">{</span>
+	FWNODE_INVALID <span class="hl opt">=</span> <span class="hl num">0</span><span class="hl opt">,</span>
+	FWNODE_OF<span class="hl opt">,</span>
+	FWNODE_ACPI<span class="hl opt">,</span>
+	FWNODE_ACPI_DATA<span class="hl opt">,</span>
+	FWNODE_PDATA<span class="hl opt">,</span>
+	FWNODE_IRQCHIP<span class="hl opt">,</span>
+<span class="hl opt">};</span>
+
+<span class="hl kwb">struct</span> fwnode_handle <span class="hl opt">{</span>
+	<span class="hl kwb">enum</span> fwnode_type type<span class="hl opt">;</span>
+	<span class="hl kwb">struct</span> fwnode_handle <span class="hl opt">*</span>secondary<span class="hl opt">;</span>
+<span class="hl opt">};</span>
+
+<span class="hl ppc">#endif</span>
+</code></pre></td></tr></table>
+</div> <!-- class=content -->
+<div class='footer'>generated by <a href='http://git.zx2c4.com/cgit/about/'>cgit v0.12</a> at 2016-02-12 17:32:00 (GMT)</div>
+</div> <!-- id=cgit -->
+</body>
+</html>
